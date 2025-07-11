6561155e3838f03ff8b0d74f3fd62a25aa045107
MilanM-2262576 <milan.muroni@student.uhasselt.be>
2025-07-11 15:36:48 +0200

    deployment

--- src/main.rs
+++ src/main.rs
@@ -56,7 +56,7 @@ async fn main() -> std::io::Result<()> {
             .service(add_block)
             .service(is_valid)
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("0.0.0.0", 8080))?  //.bind(("127.0.0.1", 8080))?
     .run()
     .await
 }
